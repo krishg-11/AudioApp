9b3ac46e70f6fac84f2e6b3a32cbcfe7030cf048
Krish <krishganotra@gmail.com>
2020-01-09 10:13:21 -0500

    button

--- app/src/main/java/traf1/ganotrakrish/audioapp/MainActivity.java
+++ app/src/main/java/traf1/ganotrakrish/audioapp/MainActivity.java
@@ -2,7 +2,11 @@ package traf1.ganotrakrish.audioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //https://www.techotopia.com/index.php/An_Android_Studio_Recording_and_Playback_Example_using_MediaPlayer_and_MediaRecorder
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        try {
+            mediaPlayer.setDataSource("AUDIO SOURCE");
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            System.out.println("playing audio");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+    protected boolean hasMicrophone() {
+        PackageManager pmanager = this.getPackageManager();
+        return pmanager.hasSystemFeature(
+                PackageManager.FEATURE_MICROPHONE);
     }
 }
