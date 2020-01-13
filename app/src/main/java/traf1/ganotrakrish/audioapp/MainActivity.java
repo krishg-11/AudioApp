58b7c2efe4355f4d4813413a5b6d28a4e02ed35c
Krish <krishganotra@gmail.com>
2020-01-13 09:30:52 -0500

    research

--- app/src/main/java/traf1/ganotrakrish/audioapp/MainActivity.java
+++ app/src/main/java/traf1/ganotrakrish/audioapp/MainActivity.java
@@ -15,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //https://www.techotopia.com/index.php/An_Android_Studio_Recording_and_Playback_Example_using_MediaPlayer_and_MediaRecorder
+        //https://developer.android.com/guide/topics/media/mediarecorder#java
+        //https://firebase.google.com/docs/storage/android/upload-files (upload from stream or upload from file)
+        //https://www.tutorialspoint.com/how-to-integrate-android-speech-to-text (no audio, just speech-to-text)
+        //https://www.androidhive.info/2014/07/android-speech-to-text-tutorial/ (pretty version of ^^)
+        //use firebase storage for mp3 storage
+
+
         MediaPlayer mediaPlayer = new MediaPlayer();
 
         try {
@@ -26,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             System.out.println(e);
         }
+        System.out.println("Microphone access: " + hasMicrophone());
+
     }
-    protected boolean hasMicrophone() {
+     boolean hasMicrophone() {
         PackageManager pmanager = this.getPackageManager();
         return pmanager.hasSystemFeature(
                 PackageManager.FEATURE_MICROPHONE);
